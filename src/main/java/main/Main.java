d5c5bde31949931e6b79ae4a8543f9fe457c3df1
jsjg73 <kjs930703@naver.com>
2021-06-05 16:52:39 +0900

    각 간선의 real path 요청을 위한 클래스 추가
    기존 LMUN 프로젝트(우리지금 만나)에서 naver api 활용 코드 copy and
    write

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import com.mycom.navigation.bus.BusInfra;
+import com.mycom.navigation.bus.reader.BusInfraTxtReader;
+import com.mycom.navigation.bus.writer.BusInfraTxtWriter;
+import com.mycompany.myapp.naver.NPath;
+import com.mycompany.myapp.naver.NaverAPI;
+
+public class Main {
+	// C:\workspace\practice\ReadExcelFile\BusStation.xlsx
+	public static void main(String[] args) {
+		
+		BusInfra bif = new BusInfraTxtReader().createBusInfra();
+//		new BusInfraTxtWriter().writeEdges(bif);;
+		new BusInfraTxtWriter().writePath(6000, bif.getBusStationTbl());
+		System.out.println("done");
+		// 7.3	MB
+//		NPath np = new NPath();
+//		100100124	0017	7	102000216	03310	남이장군사당	126.9579568939	37.5367069612
+//		100100124	0017	8	102000218	03312	새마을금고	126.9595027769	37.5371259582
+		
+//		중앙대후문	126.9535640433	37.5050079316
+//		이화약국	126.9504762701	37.50495608
+
+//		np.setEx("126.9504762701");
+//		np.setEy("37.50495608");
+//		np.setSx("126.9535640433");
+//		np.setSy("37.5050079316");
+//		NaverAPI naver = new NaverAPI();
+//		naver.getPath(np);
+//		
+//		System.out.println(np.getRawPath());
+	}
+
+}
