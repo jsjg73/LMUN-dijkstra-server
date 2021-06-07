bdeec71e6fcf8462bbcdebe968778c0540ef8da1
jsjg73 <kjs930703@naver.com>
2021-06-07 16:02:29 +0900

    todo 수정, main.java 수정, 버스정류장 구역배열 txt 생성

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,35 +1,28 @@
 package main;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import com.mycom.navigation.bus.BusInfra;
+import com.mycom.navigation.bus.dto.BusStation;
 import com.mycom.navigation.bus.reader.BusInfraTxtReader;
 import com.mycom.navigation.bus.writer.BusInfraTxtWriter;
+import com.mycompany.myapp.naver.Distance;
 import com.mycompany.myapp.naver.NPath;
 import com.mycompany.myapp.naver.NaverAPI;
 
 public class Main {
-	// C:\workspace\practice\ReadExcelFile\BusStation.xlsx
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		// 버스 인프라 구축
 		BusInfra bif = new BusInfra(new BusInfraTxtReader());
-//		new BusInfraTxtWriter().writeEdges(bif);;
-//		new BusInfraTxtWriter().writePath(6000, bif.getBusStationTbl());
-		System.out.println("done");
-		// 7.3	MB
-//		NPath np = new NPath();
-//		100100124	0017	7	102000216	03310	남이장군사당	126.9579568939	37.5367069612
-//		100100124	0017	8	102000218	03312	새마을금고	126.9595027769	37.5371259582
 		
-//		중앙대후문	126.9535640433	37.5050079316
-//		이화약국	126.9504762701	37.50495608
-
-//		np.setEx("126.9504762701");
-//		np.setEy("37.50495608");
-//		np.setSx("126.9535640433");
-//		np.setSy("37.5050079316");
-//		NaverAPI naver = new NaverAPI();
-//		naver.getPath(np);
-//		
-//		System.out.println(np.getRawPath());
+		// 버스 정류장 영역 구분 
+		// 500칸*500칸, (대략, 가로 140m * 세로 120m)
+		bif.dividingIntoArea(500, 500);
+		
+		//다익스트라
 	}
 
 }
