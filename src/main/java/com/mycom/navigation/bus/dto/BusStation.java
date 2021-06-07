d5bccf03cb86de86c9e14bcc26ab758a2bad57cd
jsjg73 <kjs930703@naver.com>
2021-06-07 15:59:04 +0900

    구역화에 필요한 필드 추가

--- src/main/java/com/mycom/navigation/bus/dto/BusStation.java
+++ src/main/java/com/mycom/navigation/bus/dto/BusStation.java
@@ -11,19 +11,26 @@ import lombok.Setter;
 @Setter
 @Builder
 public class BusStation {
-	
+	private int idx;
 	private Bus bus;
 	private String nodeId;
 	private String arsId;
 	private String name;
-	private String x;
-	private String y;
+	private double x;
+	private double y;
 	private Set<BusStation> nextSet;
+	private Set<BusStation> arroundSet;
+	private int sectionIndex;
 	
-	public boolean add(BusStation station) {
+	public boolean addNext(BusStation station) {
 		if(station == null)return false;
 		if(nextSet == null)nextSet = new HashSet<BusStation>();
 		return nextSet.add(station);
+	}
+	public boolean addArround(BusStation station) {
+		if(station == null)return false;
+		if(arroundSet == null)arroundSet = new HashSet<BusStation>();
+		return arroundSet.add(station);
 	}
 	
 }
