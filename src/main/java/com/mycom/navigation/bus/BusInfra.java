81b8ced5f6c0c8555ab8d3eefef5b0d70a55f20a
jsjg73 <kjs930703@naver.com>
2021-06-07 15:53:50 +0900

    feat : 버스정류장 구역화
    dividingIntoArea 메서드 추가
    row, col을 매개변수로받는다.
    동일 영역 및 주변 영역의 정류장까지 걸어서 환승 할 수 있는 경우를
    빠르게 확인할 수 있음

--- src/main/java/com/mycom/navigation/bus/BusInfra.java
+++ src/main/java/com/mycom/navigation/bus/BusInfra.java
@@ -1,7 +1,14 @@
 package com.mycom.navigation.bus;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -9,6 +16,8 @@ import com.mycom.navigation.bus.dto.Bus;
 import com.mycom.navigation.bus.dto.BusStation;
 import com.mycom.navigation.bus.dto.Edge;
 import com.mycom.navigation.bus.reader.BusInfraReader;
+import com.mycom.navigation.bus.writer.BusInfraTxtWriter;
+import com.mycompany.myapp.naver.Distance;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,7 +26,13 @@ import lombok.Setter;
 
 /*
  * 서울 버스노선별 정류소 정보를 포함한 버스 인프라
+ * 역할:
+ * 버스, 정류장, 간선 객체 생성
  * 버스 네트워크 구축
+ * 	버스 테이블에 객체 추가
+ *  버스 정류장 테이블에 객체 추가
+ *  연결된 정류장 정보(간선) 집합에 객체 추가
+ *  걸어서 환승할 정류장 정보(간선) 집합에 객체 추가
  * */
 @Getter
 @Setter
@@ -31,6 +46,13 @@ public class BusInfra {
 	private Map<String, Bus> busTbl = new HashMap<String, Bus>();
 	private Map<String, BusStation> busStationTbl = new HashMap<String, BusStation>();
 	private Set<Edge> edgeSet = new HashSet<Edge>();
+	
+	private Set<BusStation>[] sections;
+	private double minX = 999;
+	private double minY = 999;
+	private double maxX = 0;
+	private double maxY = 0;
+	private int row,col;
 	
 	@Getter(AccessLevel.NONE)
 	@Setter(AccessLevel.NONE)
@@ -54,22 +76,28 @@ public class BusInfra {
 		
 		//정류장 정보 추가
 		BusStation station=null;
+		int stationIndex =0;
 		if((station = containsBusStation(infs[NODE_ID]))==null) {
 			station = BusStation.builder()
+										.idx(stationIndex++)
 										.nodeId(infs[NODE_ID])
 										.arsId(infs[ARS_ID])
 										.name(infs[STATION_NM])
-										.x(infs[X])
-										.y(infs[Y])
+										.x(parseDouble(infs[X]))
+										.y(parseDouble(infs[Y]))
 										.build();
 			putBusStation(station);
+			minX=Math.min(minX, station.getX());
+			minY=Math.min(minY, station.getY());
+			maxX=Math.max(maxX, station.getX());
+			maxY=Math.max(maxY, station.getY());
 		}
 		
 		// 간선 정보 생성
 		if("1".equals(infs[ORDER])) {
 			preStation = station;
 		}else {
-			preStation.add(station);
+			preStation.addNext(station);
 			
 //			"C:/workspace/practice/ReadExcelFile/Edges.txt"에 저장할 때 사용
 			edgeSet.add(new Edge(preStation, station));
@@ -77,7 +105,32 @@ public class BusInfra {
 			preStation = station;
 		}
 	}
-	
+	public void arroudStations(double limit) {
+		Distance dis = new Distance();
+		List<BusStation> list = new ArrayList<BusStation>();
+		
+		for(Map.Entry<String, BusStation> entry : busStationTbl.entrySet()) {
+			list.add(entry.getValue());
+		}
+		
+		int i=0;
+		for(BusStation bs : list) {
+			i++;
+			if(i%1000==0)System.out.println(i);
+//			double sx = bs.getX();
+//			double sy = bs.getY();
+			for(BusStation bs2 : list) {
+				if(bs == bs2)continue;
+//				double ex = bs2.getX();
+//				double ey = bs2.getY();
+//				double d = dis.distance(sy, sx, ey, ex);
+//				if(d<=limit) {
+//					bs.addArround(bs2);
+//					edgeSet.add(new Edge(bs,bs2));
+//				}
+			}
+		}
+	}
 	private boolean containsBus(String busId) {
 		return busTbl.containsKey(busId);
 	}
@@ -91,4 +144,43 @@ public class BusInfra {
 		busStationTbl.put(station.getNodeId(), station);
 	}
 	
+	private double parseDouble(String v) {
+		return Double.parseDouble(v);
+	}
+	
+	
+	double XUnit;
+	double YUnit;
+	public void dividingIntoArea(int row, int col) throws IOException {
+		this.row=row;
+		this.col=col;
+		if(busStationTbl == null) {
+			throw new IllegalStateException("모든 버스 정류장 정보가 필요합니다.");
+		}
+		if(maxY==0 || maxX==0 ||minX==999|| minY==999) {
+			throw new IllegalStateException("경위도의 영역 한계값 설정이 필요합니다.");
+		}
+		
+		XUnit = ((maxX-minX)/500); // 0.0011009615529999905
+		YUnit = ((maxY-minY)/500); // 0.0016079761373999873
+
+		sections = new HashSet[(row+2)*(col+2)];
+		
+		for(Map.Entry<String , BusStation> ent : busStationTbl.entrySet()) {
+			BusStation bs = ent.getValue();
+			int sectionIdx = calSectionIndex(bs);
+			if(sections[sectionIdx] == null) {
+				sections[sectionIdx] = new HashSet<BusStation>();
+			}
+			sections[sectionIdx].add(bs);
+		}
+	}
+	private int calSectionIndex(BusStation station) {
+		
+		double tgX = station.getX();
+		double tgY = station.getY();
+		int x = (int)( (tgX-minX)/XUnit );
+		int y = (int)( (tgY-minY)/YUnit );
+		return y*500+x+1;
+	}
 }
