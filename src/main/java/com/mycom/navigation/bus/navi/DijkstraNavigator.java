b5ae6f2d2af8bf31346fd77a4d48077b41201108
jsjg73 <kjs930703@naver.com>
2021-06-24 00:33:41 +0900

    TODO 리스트 추가

--- src/main/java/com/mycom/navigation/bus/navi/DijkstraNavigator.java
+++ src/main/java/com/mycom/navigation/bus/navi/DijkstraNavigator.java
@@ -14,6 +14,20 @@ public class DijkstraNavigator extends Navigator {
 	@Override
 	public List<String> navigate(double sx, double sy, double ex, double ey) {
 		// TODO Auto-generated method stub
+
+		// 출발지 근처 노드 받아오기.
+		// 목적지 근처 노드 받아오기.
+		
+		// 다익스트라 문맥 생성
+		  // 노드 리스트
+		  // 방문 체크
+		  // 이전 위치(역추적에 사용)
+		
+		// 다익스트라 구현
+		
+		// 역추적 : 방문 노드 리스트
+		
+		// realpath 리스트 생성
 		return null;
 	}
 }
